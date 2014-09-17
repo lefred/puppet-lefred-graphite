a6df120489c6028672540da2ccbe46c7f2150f86
frederic.descamps <frederic.descamps@percona.com>
2014-09-17 08:00:31 +0200

    add config
    Signed-off-by: frederic.descamps <frederic.descamps@percona.com>

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,8 +1,27 @@
-class graphite {
+class graphite::config {
 
-  include graphite::packages
-  
-  Class['graphite::packages'] 
-   
+   $graphite_db_name     = $graphite::graphite_db_name
+   $graphite_db_engine   = $graphite::graphite_db_engine
+   $graphite_db_user     = $graphite::graphite_db_user
+   $graphite_db_password = $graphite::graphite_db_password
+   $graphite_db_host     = $graphite::graphite_db_host
+   $graphite_db_port     = $graphite::graphite_db_port
+
+   case $::osfamily {
+          'RedHat': {
+                $graphite_conf_file = "/etc/graphite-web/local_settings.py"
+          }
+          'Debian': {
+                $graphite_conf_file = "/etc/graphite/local_settings.py"
+          }
+        }
+
+
+
+        file {
+                $graphite_conf_file:
+                        ensure  => present,
+                        content => template("graphite/$:osfamily/my.cnf.erb"),
+        }
 
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,14 @@
-class graphite {
+class graphite ( $graphite_db_name = "graphite", 
+                 $graphite_db_engine = "mysql",
+                 $graphite_db_user = "graphite",
+                 $graphite_db_password = "secret",
+                 $graphite_db_host = "localhost",
+                 $graphite_db_port = "3306"
+               ) {
 
   include graphite::packages
   include graphite::service
+  include graphite::config
  
-  Class['graphite::packages'] ->  Class['graphite::service'] 
+  Class['graphite::packages'] ->  Class['graphite::config'] ->  Class['graphite::service'] 
 }
